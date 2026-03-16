0dbec9b0e0a306e45f45a8eee3f807910f09bcd1
RAMITH26 <ramithraghava@exafluence.com>
2026-03-16 14:09:08 +0530

    Test

---
+++ BSW/SRVLayer/Scheduler/Source/init_handlers.h
@@ -0,0 +1,20 @@
+
+#ifndef INIT_HANDLERS_H
+#define INIT_HANDLERS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdbool.h>
+
+void InitHandlers_InitAll(void);
+void InitHandlers_DeinitAll(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INIT_HANDLERS_H */
+
